01742ad0de670fdb6012a619b53e39aece0bd956
Sourav Roy <sroy3166@gmail.com>
2021-06-15 11:38:55 +0530

    Testing

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ setInterval(() => {
 
   // for analog clock
   // calculate the angle of the hands
-  hrotation = 30 * htime + mtime / 2 + stime / 120;
+  hrotation = 30 * htime + mtime / 2 + stime / 12;
   mrotation = 6 * mtime + stime / 10;
   srotation = 6 * stime;
 
